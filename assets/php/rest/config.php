eab45dcb5fcbf4331649df2f7aa74927bb45282f
hajdarevicharis <haris.hajdarevic@stu.ibu.edu.ba>
2025-04-07 02:08:36 +0200

    milestone 02

---
+++ assets/php/rest/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define('DB_HOST', 'localhost');        
+define('DB_NAME', 'web-programming-2025');    
+define('DB_PORT', '3310');             
+define('DB_USER', 'root');  
+define('DB_PASSWORD', '');
